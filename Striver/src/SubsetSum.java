6dd0eda0c2bc1a49024c1203dfb2f105db618c22
chinmay768 <chinmaysahani07@gmail.com>
2025-03-29 20:37:21 +0530

    Subset  Subs

--- Striver/src/SubsetSum.java
+++ Striver/src/SubsetSum.java
@@ -3,6 +3,8 @@ import java.util.Collections;
 
 public class SubsetSum {
 
+    //TC: O(2^n + n Log(n) // If we sort the sort the array before calling the recursion
+    //TC: 2^N + (2^N)log(2^N) ~= 2^N x N // If we sort the result array
     public static ArrayList<Integer> subsetSum(ArrayList<Integer> arr){
         ArrayList<Integer> result = new ArrayList<>();
         subsetSumHelper(arr, result, 0, 0);
---
+++ Striver/src/Subsets2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Subsets2 {
+
+    public static List<List<Integer>> subsetsWithoutDup(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        subsetsWithoutDupHelper(nums, result,  list, 0);
+        return result;
+    }
+
+    public static void subsetsWithoutDupHelper(int[] nums, List<List<Integer>> result, List<Integer> list, int idx){
+        result.add(new ArrayList<>(list));
+
+        for(int i = idx; i < nums.length; i++){
+            if(i > idx && nums[i - 1] == nums[i]) continue; // Skip the repeating elm
+            list.add(nums[i]);
+            subsetsWithoutDupHelper(nums,  result, list, i + 1);
+            list.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,2};
+
+        System.out.println(subsetsWithoutDup(arr));
+    }
+}
