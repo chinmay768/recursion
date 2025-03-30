ecd901c326bcb82666c8254bfbe1af4884c52c6e
chinmay768 <chinmaysahani07@gmail.com>
2025-03-30 20:03:27 +0530

    Permutations

---
+++ Striver/src/Permutations.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Permutations {
+
+    public static List<List<Integer>> permutations(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length]; // Don't user wrapper class to create array
+        permutationsHelper(nums, result, list, visited);
+
+        return result;
+    }
+
+    public static void permutationsHelper(int[] nums, List<List<Integer>> result, List<Integer> list, boolean[] visited){
+       if(list.size() == nums.length){
+           result.add(new ArrayList<>(list));
+           return;
+       }
+
+       for (int i = 0; i < nums.length; i++){
+           if(!visited[i]){
+               list.add(nums[i]);
+               visited[i] = true;
+               permutationsHelper(nums, result, list, visited);
+               list.removeLast();
+               visited[i] = false;
+           }
+       }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+
+        System.out.println(permutations(nums));
+    }
+}
--- Striver/src/Subsets2.java
+++ Striver/src/Subsets2.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Subsets2 {
 
+    //TC: O(2^n * n) // n because each time we are cloning a list and adding it to result
+    //SC: O(2^n * k) // Where k is avg size of  list
     public static List<List<Integer>> subsetsWithoutDup(int[] nums){
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
