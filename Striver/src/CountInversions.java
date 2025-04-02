20547eb644569fd60164a9e36f5cab4465da9fa9
chinmay768 <chinmaysahani07@gmail.com>
2025-04-02 23:24:43 +0530

    Count Inversions

--- Striver/src/CountInversions.java
+++ Striver/src/CountInversions.java
@@ -13,9 +13,58 @@ public class CountInversions {
         return count;
     }
 
+    public static Integer mergeSort(int[] nums, int start, int end){
+        if(start < end){
+            int mid = start + (end - start) / 2;
+
+            int leftInversionCount = mergeSort(nums, start, mid); // Will return the inversion count form left parition
+            int rightInversionCount = mergeSort(nums, mid + 1, end); // from right
+
+            int currInversionCount = merge(nums, start, mid, end);
+            return leftInversionCount + rightInversionCount + currInversionCount;
+        }
+        return 0;
+    }
+
+    public static int merge(int[] nums, int start, int mid, int end){
+        int inversionCount = 0;
+        int left = start;
+        int right = mid + 1;
+
+        int[] temp = new int[end - start + 1];
+        int k = 0;
+        while (left <= mid && right <= end){
+            if(nums[left] <= nums[right]){
+                temp[k] = nums[left];
+                k++;
+                left++;
+            }else{
+                temp[k] = nums[right];
+                inversionCount += mid - left + 1;
+                right++;
+                k++;
+            }
+        }
+
+        while (left <= mid){
+            temp[k++] = nums[left++];
+        }
+
+        while (right <= end){
+            temp[k++] = nums[right++];
+        }
+
+        for(int i = 0; i < temp.length; i++){
+            nums[start + i] = temp[i];
+        }
+
+        return inversionCount;
+    }
+
     public static void main(String[] args) {
         int[] arr = {6, 3, 5, 2, 7};
 
-        System.out.println(countInversionsNaive(arr));
+//        System.out.println(countInversionsNaive(arr));
+        System.out.println(mergeSort(arr, 0, arr.length - 1));
     }
 }
